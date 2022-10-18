c9d935e03aba27e14e14a0f6c320658069530ed6
Marine Romeu <romeumarine@gmail.com>
2022-10-18 09:41:36 +0200

    adding guard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,9 @@
 import { Routes } from '@angular/router';
 import { NftListComponent } from './market-place-module/nft-list/nft-list.component';
 import { NftDescriptionComponent } from './market-place-module/nft-description/nft-description.component';
+import { LogGuard } from './log.guard';
 
 export const ROUTES : Routes = [
   {path: '', component: NftListComponent},
-  {path: 'description/:blockchain/:contractId/:token', component: NftDescriptionComponent}
+  {path: 'description/:blockchain/:contractId/:token', component: NftDescriptionComponent, canActivate: [LogGuard]},
 ]
